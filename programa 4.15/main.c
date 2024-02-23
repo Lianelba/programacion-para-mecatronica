dde035b2dcfdb4eab443f00ba2f6ad9466658b4a
Lianelba <157316364+Lianelba@users.noreply.github.com>
2024-02-23 14:09:14 -0500

    Add files via upload

---
+++ programa 4.15/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int productoria (int);
+void main (void)
+{
+    int NUM;
+    do
+    {
+        printf("Ingresa el numero que quieres calcular la productoria: ");
+        scanf("%d", &NUM);
+    }
+    while (NUM >100 || NUM < 1);
+    printf ("\nLa productoria de %d es: %d", NUM, productoria(NUM));
+}
+int productoria (int N)
+{
+    int I, PRO = 1;
+    for (I = 1; I <= N; I++)
+        PRO *= I;
+    return (PRO);
+}
